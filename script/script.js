1b292bc9361009cb5349ec9c4ce0a42c9d9dafe1
prasanna177 <prasannachand77@gmail.com>
2023-10-01 22:19:12 +0545

    CRUD finished

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ const modal = document.getElementById('myModal');
 const submitBtn = document.getElementById('submitBtn');
 const closeBtn = document.getElementById('closeBtn');
 const cancelBtn = document.getElementById('cancelBtn');
+const saveBtn = document.getElementById('saveBtn');
+const inputElement = document.getElementById('todoInput');
+const inputDescElement = document.getElementById('todoDescription');
+  const hiddenIndex = document.getElementById("hiddenIndex");
 // const popupDel = document.getElementById('popupDel');
 
 const popUpclick = (element) => {
@@ -29,6 +33,8 @@ submitBtn.onclick = () => {
 
 addBtn.onclick = () => {
   modal.style.display = "flex";
+  saveBtn.style.display = "none";
+  submitBtn.style.display = "block";
 }
 
 
@@ -41,6 +47,30 @@ const deleteTask = (index) => {
     todoList.splice(index,1);
     renderTodo();
     console.log(todoList);
+}
+
+const editTask = (element, index) => {
+  modal.style.display = "flex";
+  const popupEl = element.parentNode;
+  popupEl.style.display = "none";
+  const inputElement = document.getElementById('todoInput');
+  const inputDescElement = document.getElementById('todoDescription');
+  // let name = inputElement.value;
+  // const description = inputDescElement.value;
+  inputElement.value = todoList[index].todoName;
+  inputDescElement.value = todoList[index].todoDescription;
+  saveBtn.style.display = "block";
+  submitBtn.style.display = "none";
+  hiddenIndex.value = index;
+  console.log(hiddenIndex.value)
+}
+
+saveBtn.onclick = () => {
+  const editIndex = hiddenIndex.value;
+  console.log(editIndex);
+  todoList[editIndex].todoName = inputElement.value;
+  todoList[editIndex].todoDescription = inputDescElement.value;
+  renderTodo();
 }
   
 window.onclick = (event) => {
@@ -63,7 +93,7 @@ function renderTodo() {
           <div class="task">
             <div class="task-name-container">${todoName}<i class="fa-solid fa-ellipsis-vertical" class="popupBtn" onclick = "popUpclick(this)"></i>
               <div class="flex-popup">
-                <div class = "popup-option"><p class="popup-option-text">Edit</p></div>
+                <div class = "popup-option" onclick = "editTask(this, ${i})"><p class="popup-option-text">Edit</p></div>
                 <div class = "popup-option" onclick = "deleteTask(${i})"><p class="popup-option-text">Delete</p></div>
                 <div class = "popup-option" onclick = "clickCancel(this)"><p class="popup-option-text">Cancel</p></div>
               </div>
@@ -73,7 +103,6 @@ function renderTodo() {
     todoHTML += html;
   }
 
-
   console.log(todoHTML);
   document.getElementById('tasks-container').innerHTML = todoHTML;
   // document.getElementById('tasks-container').innerHTML = '';
@@ -82,9 +111,7 @@ function renderTodo() {
 
 // add to todo from input
 function addTodo() {
-  const inputElement = document.getElementById('todoInput');
-  const inputDescElement = document.getElementById('todoDescription');
-  const name = inputElement.value;
+  let name = inputElement.value;
   const description = inputDescElement.value;
 
   if (name) {
@@ -102,4 +129,3 @@ function addTodo() {
   inputDescElement.value = '';
   renderTodo();
 }
-
